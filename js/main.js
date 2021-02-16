4b10aab68831203daf7f9b609e634773f9262a4b
Taras <franklampard_01@ukr.net>
2021-02-16 18:32:20 +0300

    create: fixed header and smooth scroll to veiw

--- js/main.js
+++ js/main.js
@@ -67,3 +67,19 @@ $(document).ready(function () {
     event.stopPropagation();
   });
 });
+
+// Плавний скролл по секціям
+let anchors = document.querySelectorAll('header a[href*="#"]'); //обовязково вказати де знаходиться меню
+
+for (anchor of anchors) {
+  if (anchor) {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      anchorId = this.getAttribute("href");
+      yOffset = -70; // задаете отступ
+      element = document.querySelector(anchorId); // находим элемент до которого нужно прокручивать страницу
+      y = element.getBoundingClientRect().top + window.pageYOffset + yOffset; // находим расстояние до элемента
+      window.scrollTo({ top: y, behavior: "smooth" });
+    });
+  }
+}
