815a230787287a25212a8af5e5c1bd1cc57cc5f6
Taras <franklampard_01@ukr.net>
2021-02-16 16:45:18 +0300

    create: sending the form in telegram

---
+++ telegram.php
@@ -0,0 +1,30 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$token = "1629599817:AAE5JwPvUfHwqgcCqKXMyLaz23cvIgMPpyU";
+$chat_id = "-526669109";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+  'Cообщение' => $message
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
