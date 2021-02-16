69b2c6cae7ce78eae264a9a8ecb6a7c1e8d66ac0
Taras <franklampard_01@ukr.net>
2021-02-16 16:13:50 +0300

    create: modal-contact

---
+++ js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+  // Modal
+  // Зберігаємо в перемінну кнопку з атрибутом data-modal і кнопку закриття data-close
+  const modalCall = $("[data-modal]");
+  const modalClose = $("[data-close]");
+
+  // Відкриття. При кліку на modalCall буде виконуватись функція
+  modalCall.on("click", function (event) {
+    //   event.preventDefault(); - додається коли в HTML кнопки у вигляді ссилок,
+    //  відміняє стандартну поведедінку
+
+    let $this = $(this);
+    let modalId = $this.data("modal");
+
+    //  Виклик модального вікна
+    $(modalId).addClass("show");
+    //  Додавання класу яка не дозволяє проручуват фон
+    $("body").addClass("no-scroll");
+
+    //  Затримка анімації
+    setTimeout(function () {
+      //  Анімація вкилику
+      $(modalId).find(".modal-contact__dialog").css({
+        transform: "scale(1)",
+      });
+    }, 200);
+  });
+
+  //Закриття модального вікна
+  modalClose.on("click", function (event) {
+    let $this = $(this);
+
+    //Метод закриття для кількох модальних вікон, по класу "радітєля"
+    let modalParent = $this.parents(".modal-contact");
+
+    //  Анімація зариття
+    modalParent.find(".modal-contact__dialog").css({
+      transform: "scale(0)",
+    });
+
+    //  Затримка анімації
+    setTimeout(function () {
+      //  Забираємо клас "show" у модального вікна
+      modalParent.removeClass("show");
+      $("body").removeClass("no-scroll");
+    }, 200);
+  });
+
+  //   Закриття модалки по кліку на маску
+  $(".modal-contact ").on("click", function (event) {
+    let $this = $(this);
+    //  Анімація зариття
+    $this.find(".modal-contact__dialog").css({
+      transform: "scale(0)",
+    });
+
+    //  Затримка анімації
+    setTimeout(function () {
+      //  Забираємо клас "show" у модального вікна
+      $this.removeClass("show");
+      $("body").removeClass("no-scroll");
+    }, 200);
+  });
+
+  //   Робимо так, щоб при кліку на саму модалку, наприкалд для заповнення форми вона не закривалсь
+  $(".modal-contact__dialog").on("click", function (event) {
+    event.stopPropagation();
+  });
+});
